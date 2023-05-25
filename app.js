c435edb0e8f4f67eddac0a47006c00ccd536802b
Roman <64091748+novemberrain00@users.noreply.github.com>
2023-05-25 16:42:34 +0300

    fixed app.js

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ app.use(express.urlencoded({ extended: false }))
 .use(express.json());
 app.use(cors()); 
 
+app.get('/', (req, res) => {
+  res.sendFile(__dirname, 'build', 'index.html');
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 });
